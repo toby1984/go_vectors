7a28ba83b6d18a7644cbaf362784146554603294
Tobias Gierke <tobias.gierke@code-sourcery.de>
2023-08-30 17:00:33 +0200

    wip

--- vector2/vector2.go
+++ vector2/vector2.go
@@ -18,6 +18,16 @@ func (v *Vector2) ToString() string {
 	return fmt.Sprintf("(%f,%f)", v.X, v.Y)
 }
 
+func (v *Vector2) Distance(x float32, y float32) float32 {
+	return float32(math.Sqrt(float64(v.DistanceSqrt(x, y))))
+}
+
+func (v *Vector2) DistanceSqrt(x float32, y float32) float32 {
+	dx := v.X - x
+	dy := v.Y - y
+	return dx*dx + dy*dy
+}
+
 func (v *Vector2) Add(v2 *Vector2) *Vector2 {
 	return &Vector2{
 		X: v.X + v2.X,
